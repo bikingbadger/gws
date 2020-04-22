c3201404b0e42e9d6edb259212f47c29673859bb
Hilton Meyer <development@hiltonmeyer.com>
2020-04-22 10:11:33 +0300

    Create a timeslot list

---
+++ src/components/TimeslotItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>
+      {{ date }} <strong>{{ customer }}</strong> {{ hours }} hours [{{
+        description
+      }}]
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: { type: String },
+    hours: { type: Number },
+    customer: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+  methods: {
+    selectNote() {
+      this.$emit('select-timeslot', this.desc);
+    },
+  },
+};
+</script>
+
+<style></style>
