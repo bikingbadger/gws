75a87d23c4ec1ac32d4c02741c45fce61a9646a0
Hilton Meyer <development@hiltonmeyer.com>
2020-04-22 08:25:11 +0300

    Create vue application for front end

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Komit logo" src="./assets/logo.png">
+    <Timeslot/>
+  </div>
+</template>
+
+<script>
+import Timeslot from './components/Timeslot'
+
+export default {
+  name: 'App',
+  components: {
+    Timeslot
+  }
+}
+</script>
+
+<style>
+
+</style>
